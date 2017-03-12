49b9060d7ed7f6e1f52b9c510c39bc35588132da
Jose Martins <lluismmartins7@gmail.com>
2017-03-12 12:02:11 +0000

    intelij files

---
+++ src/apps/BackupClient.java
@@ -0,0 +1,16 @@
+package apps;
+
+
+
+
+public class BackupClient {
+
+
+    public static void main(String args[]) throws Exception {
+
+        System.out.print("CLient");
+
+    }
+
+
+}
---
+++ src/apps/BackupService.java
@@ -0,0 +1,11 @@
+package apps;
+
+
+
+public class BackupService {
+
+    public static void main(String args[]) throws Exception {
+        System.out.print("Server");
+    }
+
+}
