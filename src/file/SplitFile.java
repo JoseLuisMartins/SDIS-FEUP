121424892721fd999a8fc85dba7b2dd4f85f3c6d
mferreira96 <pount@sapo.pt>
2017-03-17 13:01:52 +0000

    updated chunks with proper fileID and created a better organization of files

--- src/file/SplitFile.java
+++ src/file/SplitFile.java
@@ -1,6 +1,9 @@
 package file;
 
 
+import logic.Utils;
+
+import javax.swing.text.Utilities;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -11,11 +14,14 @@ public class SplitFile {
 
     private File file;
     private ArrayList<Chunk> chunksList;
+    private Utils util;
+    private String fileId;
 
     public SplitFile(File file) throws IOException {
 
         this.file = file;
         this.chunksList = new ArrayList<Chunk>();
+        this.util = new Utils();
 
         split();
     }
@@ -23,7 +29,10 @@ public class SplitFile {
     private void split() throws IOException {
 
         byte[] data = loadFile(this.file);
+        this.fileId = this.util.sha256(this.file.getName(),this.file.lastModified(),1);
         splitData(data);
+
+
     }
 
     // loads the data of the file to an array of bytes
@@ -47,14 +56,14 @@ public class SplitFile {
         int chunkNo = 0;
         // all the chunks should be with the 64KByte, except the last one that could store less.
         for (int i = 0; i < data.length - x + 1; i += x) {
-            Chunk chunk = new Chunk("teste", chunkNo, Arrays.copyOfRange(data, i, i + x), 3);
+            Chunk chunk = new Chunk(this.fileId, chunkNo, Arrays.copyOfRange(data, i, i + x), 3);
             chunksList.add(chunk);
             chunkNo++;
         }
 
 
         if (data.length % x != 0) {
-            Chunk ck = new Chunk("teste", chunkNo, Arrays.copyOfRange(data, data.length - data.length % x, data.length), 3);
+            Chunk ck = new Chunk(this.fileId, chunkNo, Arrays.copyOfRange(data, data.length - data.length % x, data.length), 3);
             chunksList.add(ck);
         }
     }
