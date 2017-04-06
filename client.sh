a9b4c8b9efc1f526b2bff8b43f497536e56ae42f
Marcelo Diocleciano Rodrigues Ferreira <up201405323@B02AU0911.fe.up.pt>
2017-04-06 21:26:42 +0100

    added scripts and gitignore

---
+++ client.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd bin
+
+java cli.BackupClient $1 $2 $3 $4
--- compile.sh
+++ compile.sh
@@ -1,26 +1,9 @@
 #!/bin/bash
 
 
-echo "script initiated."
-
 mkdir -p bin
 
-#javac -d bin -sourcepath src $(find . -name \*.java)
-javac -d bin -sourcepath src  src/cli/BackupClient.java  src/cli/BackupService.java
-
-cd bin
-
-x=1
-
-
-while [ $x -le $1 ]
-do
-	gnome-terminal -x bash -c "java cli.BackupService 1.0 $x $x 224.0.0.1 2222  224.0.0.2 2223 224.0.0.0 2224; exec bash"
-	x=$(( $x + 1 ))
-
-	wait
-
-done
+javac -d bin -sourcepath src $(find . -name \*.java)
 
 
 
